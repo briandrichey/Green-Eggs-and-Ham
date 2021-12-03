c004e8499c8847b27d915ad9cce9ae600c1449b1
mmirasol17 <78987283+mmirasol17@users.noreply.github.com>
2021-12-03 07:30:29 -0800

    Update DataClean.py

--- DataClean.py
+++ DataClean.py
@@ -1,6 +1,15 @@
 # regex library
 import regex
 
+import astral
+
+#from astropy.coordinates import get_sun, AltAz, EarthLocation
+#from astropy.time import Time
+#import astropy as ast
+#import astropy.units as u
+#from astropy.time import Time
+#from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+
 # scikit libraries
 from sklearn import preprocessing
 import numpy as np
@@ -15,6 +24,74 @@ import sys
 
 # upload libraries for pandas
 import pandas as pd
+
+# upload libraries for timing the processing time
+import time
+
+import datetime
+from timezonefinder import TimezoneFinder
+import pytz
+
+import math
+
+###########################################################
+# function that will process a line by                    #
+# separating its components with commas                   #
+#                                                         #
+# li is the current line of data that is being processed. #
+###########################################################
+def sunpos(when, location):
+    # Extract the passed data
+    year, month, day, hour, minute, second = when
+    latitude, longitude = location
+
+    tf = TimezoneFinder()
+
+    # From the lat/long, get the tz-database-style time zone name (e.g. 'America/Vancouver') or None
+    timezone_str = tf.timezone_at(lng = longitude, lat = latitude)
+    timezone = pytz.timezone(timezone_str).localize(datetime.datetime(year, day, month)).strftime("%z")
+
+    # Math typing shortcuts
+    rad, deg = math.radians, math.degrees
+    sin, cos, tan = math.sin, math.cos, math.tan
+    asin, atan2 = math.asin, math.atan2
+    # Convert latitude and longitude to radians
+    rlat = rad(latitude)
+    rlon = rad(longitude)
+    # Decimal hour of the day at Greenwich '''- timezone ''' (betw hour and min)
+    locationtime = hour - int(timezone) + minute / 60 + second / 3600
+    # Days from J2000, accurate from 1901 to 2099
+    daynum = (367 * year - 7 * (year + (month + 9) // 12) // 4 + 275 * month // 9 + day - 730531.5 + locationtime / 24)
+    # Mean longitude of the sun
+    mean_long = daynum * 0.01720279239 + 4.894967873
+    # Mean anomaly of the Sun
+    mean_anom = daynum * 0.01720197034 + 6.240040768
+    # Ecliptic longitude of the sun
+    eclip_long = (mean_long + 0.03342305518 * sin(mean_anom) + 0.0003490658504 * sin(2 * mean_anom))
+    # Obliquity of the ecliptic
+    obliquity = 0.4090877234 - 0.000000006981317008 * daynum
+    # Right ascension of the sun
+    rasc = atan2(cos(obliquity) * sin(eclip_long), cos(eclip_long))
+    # Declination of the sun
+    decl = asin(sin(obliquity) * sin(eclip_long))
+    # Local sidereal time
+    sidereal = 4.894961213 + 6.300388099 * daynum + rlon
+    # Hour angle of the sun
+    hour_ang = sidereal - rasc
+    # Local elevation of the sun
+    elevation = asin(sin(decl) * sin(rlat) + cos(decl) * cos(rlat) * cos(hour_ang))
+    # Local azimuth of the sun
+    azimuth = atan2(-cos(decl) * cos(rlat) * sin(hour_ang), sin(decl) - sin(rlat) * sin(elevation),)
+    
+    # Convert azimuth and elevation to degrees
+    azimuth = deg(azimuth) % 360 #, 0, 360)
+    elevation = deg(elevation) #, -180, 180)
+    # make sure that the range of the elevation is between -180 and 180
+    if elevation > 180: elevation = elevation - 360
+
+    # Return azimuth and elevation in degrees
+    return (round(azimuth, 2)), (round(elevation, 2))
+
 
 ###########################################################
 # function that will process a line by                    #
@@ -33,6 +110,42 @@ def process(li):
 
     #the list that will hold the processed data
     processedLineList = []
+    #the list that will hold callsigns
+    callsigns = ""
+
+    #indicates whether there is a message or not in the line of data
+    messageExists = False
+    #get the last piece of data of the line of data to determine whether this is a message or not
+    lastDataPiece = dataList[len(dataList) - 1]
+    #indicates what type of message the last piece of data is if it is a message
+    messageType = ""
+    #counts how many callsigns are within a line of data
+    callsignCounter = 0
+    #indicates whether a grid location exists or not
+    gridLocExists = False
+
+    #if the message piece is a special message
+    if (len(lastDataPiece) == 3 and lastDataPiece == "RRR"):
+        messageExists = True
+        messageType = "special"
+    elif (len(lastDataPiece) == 2 and lastDataPiece == "73"):
+        messageExists = True
+        messageType = "special"
+    elif (len(lastDataPiece) == 4 and lastDataPiece == "RR73"):
+        messageExists = True
+        messageType = "special"
+    #if the message piece is a recieved strength
+    elif (len(lastDataPiece) == 3 and (lastDataPiece[0] == '-' or lastDataPiece[0] == '+') and lastDataPiece[1].isdigit and lastDataPiece[2].isdigit):
+        messageExists = True
+        messageType = "rec_str"
+    #if the message piece looks like (ex: R-34)
+    elif (len(lastDataPiece) == 4 and lastDataPiece[0] == 'R' and (lastDataPiece[1] == '-' or lastDataPiece[1] == '+') and lastDataPiece[2].isdigit() and lastDataPiece[3].isdigit()):
+        messageExists = True
+        messageType = "encoded"
+    #if the message piece is the grid location
+    elif (len(lastDataPiece) == 4 and lastDataPiece[0].isalpha() and lastDataPiece[1].isalpha() and lastDataPiece[2].isdigit() and lastDataPiece[3].isdigit()):
+        messageExists = True
+        messageType = "grid_loc"
 
     #go through each data piece in the current line being processed
     for i in range(len(dataList)):
@@ -43,13 +156,17 @@ def process(li):
         if i < 7:
             #if there is an underscore in the current data
             #piece, the data needs to be further separated
-            if dataPiece.find("_"):
+            if i == 0 and dataPiece.find("_"):
                 #split the data where the underscore is
                 separatedData = dataPiece.split("_")
-                    
-                #store the separated data peices into the processed line
-                for sepDataPiece in separatedData:
-                    processedLineList.append(sepDataPiece)
+                
+                #get the date and add it to the processed line list in a presentable format
+                date = separatedData.pop(0)
+                #converting date to UTC
+                processedLineList.append("20" + date[0:2] + "-" + date[4:6] + "-" + date[2:4])
+                #get the time and add it to the processed line list in a presentable format
+                time = separatedData.pop(0)
+                processedLineList.append(time[0:2] + ":" + time[2:4] + "." + time[4:6])
             
             #if there is no underscore, the data piece is processed normally
             else:
@@ -57,6 +174,7 @@ def process(li):
 
         #if the data piece is apart of the payload
         else:
+            '''
             #if the current payload data piece is not the last piece of data (grid location)
             #if i != (len(dataList) - 1):
             if len(dataPiece) >= 2:
@@ -70,28 +188,22 @@ def process(li):
             #find the index of the location in the dataframe
             #locationIndex = locDf.columns.get_loc()
             locationIndex = -1
-            message = False
+            '''
 
             #if the message piece is a special message
-            if (i == len(dataList) - 1) and len(dataPiece) == 3 and dataPiece == "RRR":
-                processedLineList.append("All recieved")
-                message = True
-            elif (i == len(dataList) - 1) and len(dataPiece) == 2 and dataPiece == "73":
-                processedLineList.append("Goodbye")
-                message = True
-            elif (i == len(dataList) - 1) and len(dataPiece) == 4 and dataPiece == "RR73":
-                processedLineList.append("All recieved best wishes and thanks for the QSO")
-                message = True
+            if (i == len(dataList) - 1) and messageType == "special": #len(dataPiece) == 3 and dataPiece == "RRR":
+                processedLineList.append(dataPiece)
             #if the message piece is a recieved strength
-            elif (i == len(dataList) - 1) and len(dataPiece) == 3 and (dataPiece[0] == '-' or dataPiece[0] == '+') and dataPiece[1].isdigit and dataPiece[2].isdigit:
+            elif (i == len(dataList) - 1) and messageType == "rec_str": #len(dataPiece) == 3 and (dataPiece[0] == '-' or dataPiece[0] == '+') and dataPiece[1].isdigit and dataPiece[2].isdigit:
                 processedLineList.append(dataPiece)
-                message = True
             #if the message piece looks like (ex: R-34)
-            elif (i == len(dataList) - 1) and len(dataPiece) == 4 and dataPiece[0] == 'R' and (dataPiece[1] == '-' or dataPiece[1] == '+') and dataPiece[2].isdigit() and dataPiece[3].isdigit():
+            elif (i == len(dataList) - 1) and messageType == "encoded": #len(dataPiece) == 4 and dataPiece[0] == 'R' and (dataPiece[1] == '-' or dataPiece[1] == '+') and dataPiece[2].isdigit() and dataPiece[3].isdigit():
                 processedLineList.append(dataPiece)
-                message = True
             #if the message piece is the grid location
-            elif (i == len(dataList) - 1) and len(dataPiece) == 4 and dataPiece[0].isalpha() and dataPiece[1].isalpha() and dataPiece[2].isdigit() and dataPiece[3].isdigit():
+            elif (i == len(dataList) - 1) and messageType == "grid_loc": #len(dataPiece) == 4 and dataPiece[0].isalpha() and dataPiece[1].isalpha() and dataPiece[2].isdigit() and dataPiece[3].isdigit():
+                #since the message is the grid location, the grid location exists on this line of data
+                gridLocExists = True
+
                 #solving for latitude-----------------------------------------------
                 step1Result = (ord(dataPiece[1]) - 65) * 10
                 step2Result = ord(dataPiece[3]) - 48
@@ -101,8 +213,94 @@ def process(li):
                 step2Result = (ord(dataPiece[2]) - 48) * 2
                 longitude = (step1Result + step2Result) - 180
                 #output add the grid coordinates into the processed data
-                processedLineList.append("(" + str(latitude) + ", " + str(longitude) + ")")
-                message = True
+                processedLineList.append(str(latitude) + " " + str(longitude))
+
+                date = processedLineList[0]
+                time = processedLineList[1]
+
+                #year, month, day, hour, minute, second = when
+                when = [int(date[0:4]), int(date[5:7]), int(date[8:10]), int(time[0:2]), int(time[3:5]), int(time[6:8])]
+                #latitude, longitude = location
+                location = [latitude, longitude]
+
+                #calculate the sun data given the date, time, and location
+                sun_azimuth, sun_elevation = sunpos(when, location)
+
+                #add the sun data to the processed line
+                processedLineList.append(str(sun_azimuth))
+                processedLineList.append(str(sun_elevation))
+
+                '''
+                date = processedLineList[0]
+                time = processedLineList[1]
+
+                #if there is a 0 in front of the month, get rid of it for the UTC time
+                if date[8] == '0':
+                    date.replace('0', '')
+
+                #get the UTC time for getting the location of the sun
+                datetime = date + " " + time[0:5]
+
+                sunPos = astral.sun(date, latitude, longitude)
+                print(sunPos)
+
+                
+                loc = EarthLocation(latitude, longitude)
+                altazframe = AltAz(obstime = datetime, location = loc)
+                sunaltaz = get_sun(datetime).transform_to(altazframe)
+
+                print(sunaltaz)
+
+                
+                #get the az (direction of sun), alt (altitude of sun)
+                az, alt = ast.sun_az_alt(datetime, longitude, latitude)
+                #get the sun time (if the sun is up in the area and time or not, etc)
+                sunTime = ast.sun_time(datetime, longitude, latitude, alt, az)
+
+                processedLineList.append(az)
+                processedLineList.append(alt)
+                processedLineList.append(sunTime)
+
+                
+                #get the sun time by using the UTC time
+                sun_time = Time(UTC_time)
+                #get the Earth location from the coordinates
+                loc = EarthLocation.of_address(latitude, longitude)
+                altaz = AltAz(obstime = sun_time, location = loc)
+                zen_ang = get_sun(sun_time).transform_to(altaz).zen
+
+                processedLineList.append(sun_time)
+                processedLineList.append(zen_ang)
+                '''
+
+            #if the message piece is a callsign
+            else:
+                
+                #if the current callsign being accessed is the last callsign, store it into the callsign list and add this list to the processed line list
+                if ((i == len(dataList) - 1) and messageExists == False) or ((i == len(dataList) - 2) and messageExists == True):
+                    callsigns = callsigns + dataPiece
+                    callsignCounter = callsignCounter + 1
+                    processedLineList.append(callsigns)
+                    processedLineList.append(str(callsignCounter) + "_callsigns")
+
+                    #if the message does not exist, include nan
+                    if messageExists == False:
+                        processedLineList.append("nan")
+
+                #if the current callsign is not the last callsign, store it into the callsign list
+                else:
+                    callsignCounter = callsignCounter + 1
+                    callsigns = callsigns + dataPiece + " "
+
+                '''
+                #check through the location dataframe to see if the location prefix exists
+                for i in range(len(locDf)):
+                    #if the location prefix was found in the panda, 
+                    # get the index of the row it was found on
+                    if locDf.iloc[i]["series_prefix"] == locationPrefix:
+                        locationIndex = i
+                '''
+            '''
             #check if the first character could be dedicated for a single country
             elif locationPrefix[0] == 'N' or locationPrefix[0] == 'W' or locationPrefix[0] == 'K':
                 location = "United States of America"
@@ -125,9 +323,10 @@ def process(li):
             #if the callsign looks like <...>, then no location was found
             elif locationPrefix[0] == ".":
                 pass
-            #if the callsign is CQ, then it represents all stations
+            
+            #if the callsign is CQ, then it represents calling all stations
             elif len(dataPiece) == 2 and locationPrefix == "CQ":
-                location = "All Stations"
+                location = "Calling all Stations"
                 locationIndex = 0
             else:
                 #check through the location dataframe to see if the location prefix exists
@@ -136,6 +335,7 @@ def process(li):
                     # get the index of the row it was found on
                     if locDf.iloc[i]["series_prefix"] == locationPrefix:
                         locationIndex = i
+            
 
             #if we are not yet using the message piece of the data, it has to be a callsign
             if message == False:
@@ -149,7 +349,7 @@ def process(li):
                 #otherwise, no location was found in the panda
                 else:
                     processedLineList.append("NO LOCATION FOUND")
-            '''
+            
             if the current payload data piece is a message
             else:
                 if len(dataPiece) == 4 and dataPiece[0].isalpha() and dataPiece[1].isalpha() and dataPiece[2].isdigit() and dataPiece[3].isdigit():
@@ -169,6 +369,10 @@ def process(li):
                 else:
                     processedLineList.append(dataPiece)
             '''
+    #if the grid location was not included in this line of data, there was no sun data
+    if gridLocExists == False:
+        processedLineList.append("nan")
+        processedLineList.append("nan")
 
     processedLineList.pop(3) #getting rid of Rx/Tx
     processedLineList.pop(3) #getting rid of FT4/FT8
@@ -177,6 +381,8 @@ def process(li):
     #use to omit the payload
     #while(len(processedLineList) > 6):
         #processedLineList.pop(6)
+
+    print(processedLineList)
 
     #return the processed line list after it is cleaned up
     return processedLineList
@@ -192,7 +398,7 @@ def printToFile(listToPrint):
     print("\n... printing data to csv file ...\n")
 
     #open the csv file that will store the cleaned data
-    file = open("ham_data.csv", "w")
+    file = open("ham_data_reduced.csv", "w")
 
     #go through each line of data to separate
     #each data piece in each line with a comma
@@ -202,13 +408,34 @@ def printToFile(listToPrint):
             #only separate data peices with a comma 
             #if the current data peice is not the last one
             if i != ( len(line) - 1 ):
+                '''
+                #if the current piece of data is not a list, output it normally
+                if(type(line[i]) != list):
+                '''
                 print(line[i] + ",", end = " ")
-                file.write(line[i] + ", ")
+                file.write(line[i] + ", ") 
+                '''
+                #otherwise, the current piece of data is a list, so print the contents in list format
+                else:
+                    listOfData = line[i]
+
+                    #print("[", end = "")
+                    #file.write("[")
+
+                    for j in range(len(listOfData) - 1):
+                        print(listOfData[j], end = " ")
+                        file.write(listOfData[j] + " ") 
+
+                    print(listOfData[len(listOfData) - 1] + ",", end = " ")
+                    file.write(listOfData[len(listOfData) - 1] + ", ") 
+                '''
+
             #otherwise, the last data peice will
             #be processed with no comma after
             else:
                 print(line[i])
                 file.write(line[i] + "\n")
+
 
     #close the csv file since the cleaned data has been stored
     file.close()
@@ -245,6 +472,8 @@ def main():
             #it will print the data sets
             lines = hamfile.readlines()
 
+            #start timer to time how long it takes to process the data
+            tic = time.perf_counter()
             #process each line of the data
             for line in lines:
                 #process the current line of data by passing to the process func
@@ -252,6 +481,10 @@ def main():
                 #store previously processed line (list of data pieces) 
                 #into the processed lines matrix
                 processedLinesMatrix.append(processedLine)
+            #end the timer to get how long it took to process the data
+            toc = time.perf_counter()
+            #show how long it took to process the data
+            print(f"The data took {toc - tic:0.4f} seconds to process")
 
             #ask the user if they want to save the processed data into a csv file
             print("Do you want to save the processed data in a csv file? (yes/no): ")
